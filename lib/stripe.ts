6fb2e4e3191705051fafe5ed83297aca3f2eb2a1
Mohammed Hariss <mail.mohammedhariss@gmail.com>
2026-05-07 14:12:54 +0530

    fix: bump Stripe apiVersion to 2025-02-24.acacia

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,8 +2,7 @@ import Stripe from 'stripe'
 import { prisma } from './db'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
-  typescript: true,
+  apiVersion: '2025-02-24.acacia',
 })
 
 export async function getOrCreateStripeCustomer(userId: string): Promise<string> {
